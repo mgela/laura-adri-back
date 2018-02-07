a250df6c06910bfa6a91c93939b4a62b09d17e8a
MarkyMark <mgelabert3@gmail.com>
2018-02-07 23:26:15 +0100

    mail service completed

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,25 +1,25 @@
 'use strict';
-// const nodemailer = require('nodemailer');
-// var sgTransport = require('nodemailer-sendgrid-transport');
+
+
 const sgMail = require('@sendgrid/mail');
-
-
-
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 
 
 exports.boda = async (req, res) => {
 
-
-
-     const msg = {
-       to: 'lauraiadri1@gmail.com',
-       from: 'test@example.com',
-       subject: 'Sending with SendGrid is Fun',
-       text: 'and easy to do anywhere, even with Node.js',
-       html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-     }
+  const msg = {
+    to: 'lauraiadri1@gmail.com',
+    from: 'rsvp@lauraiadri.com',
+    subject: `${req.body.nom} vindrà amb: ${req.body.acompanyant}`,
+    html: ' ',
+  }
+  
+   try{
      sgMail.send(msg);
 
-  
+     res.sendStatus(200);
+   } catch (e){
+     res.sendStatus(500);
+   }
 }
