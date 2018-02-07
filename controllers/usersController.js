ee6b891c3917b839336ad517d7450203a6f0f90f
MarkyMark <mgelabert3@gmail.com>
2018-02-07 13:13:21 +0100

    environment vars and sendgrid

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,13 +1,35 @@
 'use strict';
+const nodemailer = require('nodemailer');
+var sgTransport = require('nodemailer-sendgrid-transport');
+
+
+
+
+
+
+  const sgMail = require('@sendgrid/mail');
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
+
+
 
 
 exports.boda = async (req, res) => {
-  console.log("heyyy");
-   // try{
-   //   console.log(23);
-   //
-   //   res.sendStatus(200);
-   // } catch (e){
-   //   res.sendStatus(500);
-   // }
+  const msg = {
+    to: 'lauraiadri1@gmail.com',
+    from: 'test@example.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+  
+   try{
+     sgMail.send(msg);
+
+     res.sendStatus(200);
+   } catch (e){
+     res.sendStatus(500);
+   }
 }
